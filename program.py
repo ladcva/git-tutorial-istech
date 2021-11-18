9cd8de480ff20caf631752501d79d59c9948524e
ladcva <ladcva@yahoo.com>
2021-11-18 22:37:26 +0700

    change python.py add get age function

--- program.py
+++ program.py
@@ -7,6 +7,10 @@ def get_facebook_response():
     response = requests.get('https://www.facebook.com/')
     return response.text
 
+# Get the age of the user and print it
+def get_age():
+    age = input('What is your age? ')
+    return age
 
 print(time.time())
 print(get_facebook_response())
